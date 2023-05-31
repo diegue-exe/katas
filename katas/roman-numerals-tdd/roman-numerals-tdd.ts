370caa6161d8d9e5410ff87200bf149eb60907c3
diegue-exe <dieeegue2104@gmail.com>
2023-05-31 13:58:56 +0200

    feat: Add 'Roman Numerals' kata solution applying TDD

---
+++ katas/roman-numerals-tdd/roman-numerals-tdd.ts
@@ -0,0 +1,16 @@
+/* https://www.codurance.com/es/katalyst/numeros-romanos */
+
+import { NUMERAL_TO_ROMAN } from "./consts";
+
+export const romanNumeralConverter = (numberToConvert: number) => {
+  let remainingToConvert = numberToConvert;
+  let romanNumber = "";
+
+  NUMERAL_TO_ROMAN.forEach(({ numeral, roman }) => {
+    while (remainingToConvert >= numeral) {
+      romanNumber += roman;
+      remainingToConvert -= numeral;
+    }
+  });
+  return romanNumber;
+};
