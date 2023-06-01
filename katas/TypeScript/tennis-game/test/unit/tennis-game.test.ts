5b7130cf09e48be21a8b22b835b48d32e343de3b
diegue-exe <dieeegue2104@gmail.com>
2023-06-01 09:39:47 +0200

    fix: Change 'displayScore' test to display the correct message

--- katas/TypeScript/tennis-game/test/unit/tennis-game.test.ts
+++ katas/TypeScript/tennis-game/test/unit/tennis-game.test.ts
@@ -1,8 +1,8 @@
 import { displayScore } from "../../tennis-game";
 describe("checks if displays score correctly", () => {
   test("if given a numeric score displays the current score", () => {
-    expect(displayScore("0-1")).toBe("0-15");
-    expect(displayScore("0-2")).toBe("0-30");
-    expect(displayScore("1-3")).toBe("15-40");
+    expect(displayScore("0-1")).toBe("Love-Fifteen");
+    expect(displayScore("0-2")).toBe("Love-Thirty");
+    expect(displayScore("1-3")).toBe("Fifteen-Forty");
   });
 });
